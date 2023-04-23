98e9ec3f3db3e4128931aef69c19e3a0394d57f3
gokuljayavel <51689273+gokuljayavel@users.noreply.github.com>
2023-04-23 18:30:22 -0400

    gokuls frontend changes

--- projectmanagementfrontend/src/App.js
+++ projectmanagementfrontend/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store.js";
+import UpdateProject from "./components/Project/UpdateProject.js";
+import ProjectBoard from "./components/ProjectBoard/story";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/addStory.js";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/updateStory.js";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/Project/UserManagement/Register.js";
+import Login from "./components/Project/UserManagement/Login";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtil/setJWTToken.js";
+import { SET_CURRENT_USER } from "./actions/type-constant.js";
+import { logout } from "./actions/securityActions.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const jwtToken = localStorage.jwtToken;
+
+if (jwtToken) {
+  setJWTToken(jwtToken);
+  const decoded_jwtToken = jwt_decode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decoded_jwtToken
+  });
+
+  const currentTime = Date.now() / 1000;
+  if (decoded_jwtToken.exp < currentTime) {
+    store.dispatch(logout());
+    window.location.href = "/";
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            {
+              //Public Routes
+            }
+
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+            {
+              //Private Routes
+            }
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
+            <Route
+              exact
+              path="/updateProjectTask/:backlog_id/:pt_id"
+              component={UpdateProjectTask}
+            />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
